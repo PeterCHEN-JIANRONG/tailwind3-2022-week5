7f12c2d2486af79d33698fa8361a1e7de769138a
peter.chen <peter@begonia-design.com.tw>
2022-08-16 22:02:23 +0800

    add menu active

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,11 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+  // menu__item add .active
+  $(".menu .menu__item").click(function (e) {
+    e.preventDefault();
+    $(".menu .menu__item").removeClass("active");
+    $(this).addClass("active");
+  });
 });
