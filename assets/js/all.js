10cdb24c5744c8b67aced372846047acfd898f32
peter.chen <peter@begonia-design.com.tw>
2022-08-16 23:57:17 +0800

    Update 2022-08-16T15:57:15.506Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,24 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  console.log('HesSchool Hello!'); // menu__item add .active
+
+  $(".menu .menu__item").click(function (e) {
+    e.preventDefault();
+    $(".menu .menu__item").removeClass("active");
+    $(this).addClass("active");
+  }); // menu__item add .active
+
+  $(".pagination .page").click(function (e) {
+    e.preventDefault();
+    $(".pagination .page").removeClass("active");
+    $(this).addClass("active");
+  }); // modal switch open
+
+  $("#user-modal .modal-switch").click(function (e) {
+    e.preventDefault();
+    $("#user-modal .modal-switch").toggleClass("active");
+    $("#user-modal .modal-options").toggleClass("active");
+  });
 });
 //# sourceMappingURL=all.js.map
